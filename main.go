30a92a1bf33e2d06d0e3ec403a52508b1b99972a
Anar <drop.bot@inbox.ru>
2026-02-25 15:41:39 +0300

    add: readinput function

--- main.go
+++ main.go
@@ -10,9 +10,29 @@ func main() {
 
 	const eurToRub = (1 / usdToEur) * usdToRub
 
+	amount, from, to := readInput()
+	fmt.Println(amount, from, " -> ", to)
+
 	fmt.Println("--- Курсы валют ---")
 	fmt.Printf("Курс USD -> EUR: %.2f\n", usdToEur)
 	fmt.Printf("Курс USD -> RUB: %.2f\n", usdToRub)
 	fmt.Println("-------------------")
 	fmt.Printf("Рассчитанный курс EUR -> RUB: %.2f\n", eurToRub)
+}
+
+func readInput() (float64, string, string) {
+	var amount float64
+	var from string
+	var to string
+
+	fmt.Print("Введите сумму : ")
+	fmt.Scan(&amount)
+
+	fmt.Print("Введите исходную валюту (например USD): ")
+	fmt.Scan(&from)
+
+	fmt.Print("Введите целевую валюту (например EUR): ")
+	fmt.Scan(&to)
+
+	return amount, from, to
 }

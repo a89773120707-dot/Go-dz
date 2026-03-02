c50d6c6d1efe7560fd09dffbf8f6528b02ce362d
Anar <drop.bot@inbox.ru>
2026-03-02 23:11:47 +0300

    feat: улучшена логика выбора валюты

--- main.go
+++ main.go
@@ -1,38 +1,130 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+const (
+	usdToEur = 0.84
+	usdToRub = 76.75
+	eurToRub = (1 / usdToEur) * usdToRub
+)
 
 func main() {
-	const (
-		usdToEur = 0.84
-		usdToRub = 76.75
-	)
+	for {
+		amount, from, to := readInput()
 
-	const eurToRub = (1 / usdToEur) * usdToRub
+		result := convert(amount, from, to)
+		fmt.Printf("%.2f %s = %.2f %s\n", amount, from, result, to)
 
-	amount, from, to := readInput()
-	fmt.Println(amount, from, " -> ", to)
+		fmt.Println("--- Курсы валют ---")
+		fmt.Printf("Курс USD -> EUR: %.2f\n", usdToEur)
+		fmt.Printf("Курс USD -> RUB: %.2f\n", usdToRub)
+		fmt.Println("-------------------")
+		fmt.Printf("Рассчитанный курс EUR -> RUB: %.2f\n", eurToRub)
 
-	fmt.Println("--- Курсы валют ---")
-	fmt.Printf("Курс USD -> EUR: %.2f\n", usdToEur)
-	fmt.Printf("Курс USD -> RUB: %.2f\n", usdToRub)
-	fmt.Println("-------------------")
-	fmt.Printf("Рассчитанный курс EUR -> RUB: %.2f\n", eurToRub)
+		isCheckRepeat := checkRepeat()
+		if isCheckRepeat {
+			continue
+		}
+		fmt.Println("До встречи ...")
+		return
+
+	}
+}
+func checkRepeat() bool {
+	var vibor string
+	fmt.Println("Хотите продолжить ? нажмите  (y/n): ")
+	fmt.Scan(&vibor)
+	lowerVibor := strings.ToLower(vibor)
+	if lowerVibor == "y" {
+		return true
+	}
+	return false
+}
+func convert(amount float64, from, to string) float64 {
+
+	switch from {
+	case "usd":
+		if to == "eur" {
+			return amount * usdToEur
+		}
+		if to == "rub" {
+			return amount * usdToRub
+		}
+
+	case "eur":
+		if to == "usd" {
+			return amount / usdToEur
+		}
+		if to == "rub" {
+			return amount * (1 / usdToEur) * usdToRub
+		}
+
+	case "rub":
+		if to == "usd" {
+			return amount / usdToRub
+		}
+		if to == "eur" {
+			return amount / ((1 / usdToEur) * usdToRub)
+		}
+	}
+	return 0
 }
 
 func readInput() (float64, string, string) {
-	var amount float64
-	var from string
-	var to string
 
-	fmt.Print("Введите сумму : ")
-	fmt.Scan(&amount)
-
-	fmt.Print("Введите исходную валюту (например USD): ")
-	fmt.Scan(&from)
-
-	fmt.Print("Введите целевую валюту (например EUR): ")
-	fmt.Scan(&to)
+	from := inputFrom()
+	amount := inputAmount()
+	to := inputTo(from)
 
 	return amount, from, to
+}
+
+func inputTo(from string) string {
+	var to string
+	for {
+		fmt.Printf("Введите целевую валюту (кроме %s): ", strings.ToUpper(from))
+		fmt.Scan(&to)
+		lowerTo := strings.ToLower(to)
+		if lowerTo == from {
+			fmt.Println("Целевая валюта не должна совпадать с исходной!")
+			continue
+		}
+		if lowerTo == "usd" || lowerTo == "eur" || lowerTo == "rub" {
+			return lowerTo
+		}
+		fmt.Println("Введите праивльное название валюты !")
+		continue
+	}
+}
+
+func inputAmount() float64 {
+	var amount float64
+	for {
+		fmt.Println("Введите сумму [НЕ ОТРИЦАТЕЛЬНО ЧИСЛО]: ")
+		fmt.Scan(&amount)
+		if amount <= 0 {
+			fmt.Println("Число не может быть меньше или равно 0")
+			continue
+		}
+		return float64(amount)
+	}
+
+}
+
+func inputFrom() string {
+	var from string
+	for {
+		fmt.Print("Введите исходную валюту (например USD/EUR/RUB): ")
+		fmt.Scan(&from)
+		lowerFrom := strings.ToLower(from)
+		if lowerFrom == "usd" || lowerFrom == "eur" || lowerFrom == "rub" {
+			return lowerFrom
+		}
+		fmt.Println("Введите праивльное название валюты !")
+		continue
+	}
+
 }
